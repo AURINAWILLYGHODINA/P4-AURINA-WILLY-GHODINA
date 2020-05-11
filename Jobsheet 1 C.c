5fdddb27bbf3b57d34e9ff34dbdcaad934be76fa
AURINAWILLYGHODINA <64614264+AURINAWILLYGHODINA@users.noreply.github.com>
2020-05-11 21:46:32 +0800

    Create Jobsheet 1 C.c
    membuat program array satu dimensi yang menampilkan NIM masing-masing mahasiswa

---
+++ Jobsheet 1 C.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main (){
+	int nim [12]={1,2,3,4,5};
+	printf("Nama     : Aurina willy ghodina\nNim      : F1B019032\nKelompok : 6\n");
+	printf ("nim pertama=%i\n",nim [0]);
+	printf ("nim kedua=%i\n",nim [1]);
+	printf ("nim ketiga=%i\n",nim [0]);
+	printf ("nim keempat=%i\n",nim [0]);
+	printf ("nim kelima=%i\n",nim [0]);
+	printf ("nim keenam=%i\n",nim [0]);
+	printf ("nim ketujuh=%i\n",nim [0]);
+	printf ("nim kedelapan=%i\n",nim [0]);
+	printf ("nim kesembilan=%i\n",nim [0]);
+	printf ("nim kesepuluh=%i\n",nim [0]);
+	printf ("nim kesebelas=%i\n",nim [0]);
+}
