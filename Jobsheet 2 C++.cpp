b01e60a6c8f03231de75b8ee899c402a9f545d60
AURINAWILLYGHODINA <64614264+AURINAWILLYGHODINA@users.noreply.github.com>
2020-05-11 21:51:17 +0800

    Create Jobsheet 2 C++.cpp
    membuat program mengisi dan menampilkan elemen array dengan index array sesuai dengan NIM terakhir setiap mahasiswa

---
+++ Jobsheet 2 C++.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+int main ()
+{
+cout<<"Nama : Aurina willy ghodina\n";
+cout<<"Nim : F1B019032\n";
+cout<<"Kelompok: Kelompok 6\n\n";
+
+int x [12];
+cout<<"masukkan angka NIM Terakhir"<<endl;
+cout<<"masukan nim 1 : ";
+cin>>x[0];
+cout<<"masukan nim 2 : ";
+cin>>x[1];
+cout<<"masukan nim 3 : ";
+cin>>x[2];
+cout<<"masukan nim 4 : ";
+cin>>x[3];
+cout<<"masukan nim 5 : ";
+cin>>x[4];
+cout<<"masukan nim 6 : ";
+cin>>x[5];
+cout<<"masukan nim 7: ";
+cin>>x[6];
+cout<<"masukan nim 8 : ";
+cin>>x[7];
+cout<<"masukan nim 9: ";
+cin>>x[8];
+cout<<"masukan nim 10 : ";
+cin>>x[9];
+cout<<"masukan nim 11 : ";
+cin>>x[10];
+cout<<"masukan nim 12: ";
+cin>>x[11];
+return 0 ;
+}
