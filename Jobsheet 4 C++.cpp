af51f29694becf60ed5507000a816591fa7d34fb
AURINAWILLYGHODINA <64614264+AURINAWILLYGHODINA@users.noreply.github.com>
2020-05-11 21:56:42 +0800

    Create Jobsheet 4 C++.cpp
    membuat program pengurangan matriks dua dimensi dengan menggunakan NIM dua orang mahasiswa

---
+++ Jobsheet 4 C++.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+main (){
+cout<<"Nama : Aurina willy ghodina\n";
+cout<<"Nim : F1B019032\n";
+cout<<"Kelompok: Kelompok 6\n\n";
+
+int nim1[3][3];
+int nim2[3][3];
+int jml [2][2];
+int x, y;
+cout<< "PENGURANGAN MATRIX\n";
+for (x = 0; x < 2; x++){
+for (y = 0; y < 2; y++){
+cout<< "Masukkan index " << x << "," << y << " = ";
+cin>> nim1[x][y];
+}
+}
+cout<<endl;
+for (x = 0; x < 2; x++){
+for (y = 0; y < 2; y++){
+cout<< "Masukkan index " << x << "," << y << " = ";
+cin>> nim2[x][y];
+}
+}
+cout<<endl;
+for (x = 0; x < 2; x++){
+for (y = 0; y < 2; y++){
+jml [x][y] = nim1[x][y]-nim2[x][y];
+cout<<jml [x][y] <<" ";
+}
+cout<<endl;
+}
+}
